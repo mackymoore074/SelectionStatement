7b39e719a703bbc89e772dd562f273ceec7ead12
Adedapo Aluko <AlukoA@davidandmargaret.org>
2024-08-23 13:57:53 -0700

    Excerice1

--- SelectionStatement/Program.cs
+++ SelectionStatement/Program.cs
@@ -1,20 +1,4 @@
-﻿/*
- * Create a program that allows a user to play a game where they must guess what your favorite number is:
-
-Initialize an integer to represent a favorite number and assign a value with a number of your choosing or you can assign a random number to this variable:
-var r = new Random();
-var favNumber = r.Next(1,1000);
-var userInput = int.Parse(Console.ReadLine());
-The following code will allow the user to guess what your favorite number is:
-
-var userInput = int.Parse(Console.ReadLine());
-Create an if-statement that if the guessed number is below the initial value, print out “too low”.
-
-Create an else-if statement that if the number is higher than the initial value, print out “too high”.
-
-Create an else statement that prints out some type of congratulations for guessing the correct number e.g. “You guessed it!!!”.
-*/
-
+﻿
 using System;
 using System.Security.Cryptography.X509Certificates;
 
@@ -40,32 +24,6 @@ static void Main(string[] args)
 
     }
 
-
-    Console.WriteLine ("what is your favorite subject?");
-
-        string subject = Console.ReadLine();
-
-    switch (subject)
-    {
-        case "Math":
-            Console.WriteLine("Math is a great subject");
-            break;
-        case "Science":
-            Console.WriteLine("Science is a nice subject");
-            break;
-        case "History":
-            Console.WriteLine("History is a great subject to historical Heritage");
-            break;
-        case "English":
-            Console.WriteLine("English is a great subject to improve vocabulary");
-            break;
-        case"Art":
-            Console.WriteLine("Art is a great subject to show talent");
-            break;
-        default:
-            Console.WriteLine("Interesting choice! What do you like about it?");
-            break; ;
-    }
 }
 
 
